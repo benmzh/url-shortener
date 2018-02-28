6ac73b36707adf895c6250380a3bfd5b0f6e635b
benmzh <ben@webworkshop.ltd>
2018-02-28 15:17:55 +0000

    Change db name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.get('/db', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM url_table', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
